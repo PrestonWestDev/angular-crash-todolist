c882572f767666ecf3a7b4d7e579330f96809c4e
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-07-22 11:41:45 -0500

    Replaced hard-coded data with free jsonplaceholder API

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,34 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 4,
-        title: 'Todo Four',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10');
   }
 }
